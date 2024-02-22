4abd9d176c280c11ff802817cb399d77bcab5e2b
Abd Elrahman Mohamed <152323454+MoriFGC@users.noreply.github.com>
2024-02-22 14:32:28 +0100

    Add files via upload

---
+++ JS Basics 4/S3P1-scaricabile.js
@@ -0,0 +1,141 @@
+/* ESERCIZIO 1
+ Scrivi una funzione chiamata "crazySum" che riceve due numeri interi come parametri.
+ La funzione deve ritornare la somma di quei due valori, ma se il loro valore è lo stesso allora deve ritornare la loro somma moltiplicata per 3.
+*/
+
+function crazySum(numero1, numero2) {
+    if (numero1 === numero2) {
+        let valore = (numero1 + numero2) * 3;
+        return valore
+    } else {
+        let valore = numero1 + numero2;
+        return valore;
+    }
+    
+}
+
+console.log(crazySum(10, 10));
+
+
+/* ESERCIZIO 2
+ Scrivi una funzione chiamata "boundary", che accetta un numero intero come parametro e ritorna true se tale parametro è incluso tra 20 e 100 (incluso) o se è esattamente uguale a 400.
+*/
+
+function boundary(numeroIntero) {
+    if ((numeroIntero > 20 && numeroIntero <= 100) || numeroIntero === 400) {
+        return true;
+    } else {
+        return false
+    }
+}
+
+console.log(boundary(10));
+/* ESERCIZIO 3
+ Scrivi una funzione chiamata "reverseString", che accetta una stringa come parametro e la ritorna invertita (es.: EPICODE => EDOCIPE).
+*/
+
+function reverseString(stringa) {
+    return stringa.split("").reverse("").join("")
+}
+
+console.log(reverseString("EPICODE"))
+
+/* ESERCIZIO 4
+ Scrivi una funzione chiamata "upperFirst", che accetta una stringa come parametro e la ritorna rendendo maiuscola ogni lettera iniziale di ogni parola.
+*/
+function upperFirst(stringa) {
+    let parola = stringa.split(" ")
+    let finalString = [];
+    for ( let i = 0; i < parola.length; i++) {
+        let primaLettera = parola[i].charAt(0);
+        let upperCaseChar = primaLettera.toUpperCase();
+        let cuttedString = parola[i].slice(1);
+        let parolaFinale = upperCaseChar + cuttedString;
+        finalString.push(parolaFinale)
+    }
+    console.log(finalString.join(" "))
+}
+console.log(upperFirst("tiranno lolo"))
+
+/* ESERCIZIO 5
+ Scrivi una funzione chiamata "giveMeRandom", che accetta come parametro un numero chiamato n e ritorna un array contenente n numeri random contenuti tra 0 e 10.
+*/
+
+function giveMeRandom(n) {
+    let numeriCasuali = [];
+
+    for (let i = 0; i < n; i++) {
+        let numero = Math.floor(Math.random() * 11)
+        numeriCasuali.push(numero)
+    }
+    return numeriCasuali
+}
+
+console.log(giveMeRandom(10));
+
+//EXTRA:
+/* ESERCIZIO 1
+ Scrivi una funzione chiamata "area" che riceve due parametri (l1, l2) e calcola l'area del rettangolo associato.
+*/
+function area(L1, L2) {
+    return L1 * L2
+}
+console.log(area(10, 5));
+
+/* ESERCIZIO 2
+ Scrivi una funzione chiamata "crazyDiff" che calcola la differenza assoluta tra un numero fornito e 19.
+ Se il valore calcolato è più grande di 19, la funzione deve tornare tale risultato moltiplicato per 3.
+*/
+
+function crazyDiff(numero, diciannove, limite, valore) {
+   valore = numero - diciannove
+   if (valore > limite) {
+    return valore * 3
+   } else {
+    return valore
+   }
+}
+
+console.log(crazyDiff(19, 19, 19, 0))
+/* ESERCIZIO 3
+ Scrivi una funzione chiamata "codify" che accetta una stringa come parametro.
+ La funzione deve aggiungere la parola "code" all'inizio della stringa fornita e ritornare il risultato, ma se la stringa fornita comincia proprio con "code" allora deve ritornarla senza modifiche.
+*/
+
+function codify(stringa) {
+    if (stringa.startsWith("code")) {
+        return stringa
+    } else {
+        return "code" + stringa
+    }
+}
+
+console.log(codify("banana"));
+/* ESERCIZIO 4
+ Scrivi una funzione chiamata "check3and7" la quale accetta un numero intero positivo come parametro.
+ La funzione deve controllare che tale parametro sia un multiplo di 3 o di 7, e in tal caso tornare true; altrimenti deve tornare false.
+ SUGGERIMENTO: operatore modulo
+*/
+
+function check3and7(num) {
+    if (num % 3 === 0 || num % 7 === 0) {
+        return true
+    } else {
+        return false
+    }
+}
+console.log(check3and7(7));
+
+/* ESERCIZIO 5
+ Scrivi una funzione chiamata "cutString", che accetta una stringa come parametro e la ritorna senza il primo e l'ultimo carattere.
+*/
+
+function cutString(stringa) {
+    if (stringa.length >= 2) {
+        return stringa.slice(1, -1)
+    } else {
+        return "";
+    }
+}
+
+console.log(cutString("elicottero"));
